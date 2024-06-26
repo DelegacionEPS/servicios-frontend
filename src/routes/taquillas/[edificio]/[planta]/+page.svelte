ee14bd8d046087e34d059bc8a8f5a1624499aeba
Eduardo <100472175@alumnos.uc3m.es>
2024-06-26 18:38:50 +0200

    removed undefineds and added

--- src/routes/taquillas/[edificio]/[planta]/+page.svelte
+++ src/routes/taquillas/[edificio]/[planta]/+page.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import { Button, Dropdown, Radio, ButtonGroup, Toast, Popover, Indicator } from 'flowbite-svelte';
+	import {
+		Button,
+		Dropdown,
+		Radio,
+		ButtonGroup,
+		Toast,
+		Popover,
+		Indicator,
+		Card
+	} from 'flowbite-svelte';
 	import {
 		ChevronDownOutline,
 		ArrowLeftOutline,
@@ -10,6 +19,7 @@
 		QuestionCircleSolid
 	} from 'flowbite-svelte-icons';
 	import TablaTaquillas from '../../../../TablaTaquillas.svelte';
+	import { Confetti } from 'svelte-confetti';
 
 	export let data: PageData;
 
@@ -50,29 +60,44 @@
 	function handleReload() {
 		setTimeout(() => {
 			location.reload();
-		}, 2000);
+		}, 4000);
+	}
+
+	function clearForm(time: number) {
+		setTimeout(() => {
+			form = null;
+		}, time);
 	}
 
 	export let form;
 </script>
 
-<h1 class="text-3xl sm:text-4xl lg:text-5xl xl:text-7xl text-center font-montserrat mt-4 mb-4 text-dele-color dark:text-dark-primary">
+<h1
+	class="text-3xl sm:text-4xl lg:text-5xl xl:text-7xl text-center font-montserrat mt-4 mb-4 text-dele-color dark:text-dark-primary"
+>
 	Selección de Taquilla
 </h1>
 
 <div class="w-screen grid grid-cols-1 place-items-center mb-4">
-	<Button id="pop_edificio" class="dark:text-dark-primary dark:hover:text-dark-accent text-dele-color hover:text-dele-accent">
-		<QuestionCircleSolid class="md:h-8 md:w-8 h-10 w-10"/>
+	<Button
+		id="pop_edificio"
+		class="dark:text-dark-primary dark:hover:text-dark-accent text-dele-color hover:text-dele-accent"
+	>
+		<QuestionCircleSolid class="md:h-8 md:w-8 h-10 w-10" />
 	</Button>
 </div>
 
-<Popover class="text-black dark:text-white dark:bg-dark-secondary md:w-1/3 sm:w-1/2 w-10/12 sm:text-md text-sm" title="Tutorial Taquillas - Selección de Taquilla" triggeredBy="#pop_edificio">
-	Por último, tienes que buscar la taquilla seleccionando el bloque donde se encuentra la misma. Para cambiar entre bloques,
-	puedes usar el menú desplegable o los botones inferiores. Además, tienes un mapa al final de la página para ayudarte
-	a seleccionar el bloque. 
-	<br><br>	
-	<b>Recuerda hacer login en una cuenta de la UC3M para reservar la taquilla.<b>
-</Popover >
+<Popover
+	class="text-black dark:text-white dark:bg-dark-secondary md:w-1/3 sm:w-1/2 w-10/12 sm:text-md text-sm"
+	title="Tutorial Taquillas - Selección de Taquilla"
+	triggeredBy="#pop_edificio"
+>
+	Por último, tienes que buscar la taquilla seleccionando el bloque donde se encuentra la misma.
+	Para cambiar entre bloques, puedes usar el menú desplegable o los botones inferiores. Además,
+	tienes un mapa al final de la página para ayudarte a seleccionar el bloque.
+	<br /><br />
+	<b>Recuerda hacer login en una cuenta de la UC3M para reservar la taquilla.<b> </b></b></Popover
+>
 
 <div class="w-screen h-full grid grid-rows-1 place-items-center">
 	<Button
@@ -102,19 +127,35 @@
 {#if drawBlocks}
 	<div class="grid grid-cols-1 place-self-center mt-6">
 		<div class="w-auto m-auto dark:text-white grid sm:grid-cols-4 grid-cols-2">
-			<span class="flex items-center"><Indicator size="lg" color="green" class="me-1.5" />Libre</span>
-			<span class="flex items-center"><Indicator size="lg" color="yellow" class="me-1.5" />Reservada</span>
-			<span class="flex items-center"><Indicator size="lg" color="red" class="me-1.5" />Ocupada</span>
-			<span class="flex items-center"><Indicator size="lg" color="dark" class="me-1.5" />No Disponible</span>
+			<span class="flex items-center"
+				><Indicator size="lg" color="green" class="me-1.5" />Libre</span
+			>
+			<span class="flex items-center"
+				><Indicator size="lg" color="yellow" class="me-1.5" />Reservada</span
+			>
+			<span class="flex items-center"
+				><Indicator size="lg" color="red" class="me-1.5" />Ocupada</span
+			>
+			<span class="flex items-center"
+				><Indicator size="lg" color="dark" class="me-1.5" />No Disponible</span
+			>
 		</div>
 	</div>
 	<TablaTaquillas bind:ocupacion_bloques={ocupacionBloques} bind:block bind:data></TablaTaquillas>
 	<div class="w-screen h-auto grid grid-rows-1 place-items-center mt-6">
 		<ButtonGroup class="space-x-px">
-			<Button pill class="dark:bg-dark-primary dark:hover:bg-dark-accent bg-[#3BC4A0] hover:bg-[#FF6D2E]" on:click={() => substractBlock()}>
+			<Button
+				pill
+				class="dark:bg-dark-primary dark:hover:bg-dark-accent bg-[#3BC4A0] hover:bg-[#FF6D2E]"
+				on:click={() => substractBlock()}
+			>
 				<ArrowLeftOutline />
 			</Button>
-			<Button pill class="dark:bg-dark-primary dark:hover:bg-dark-accent bg-[#3BC4A0] hover:bg-[#FF6D2E]" on:click={() => addBlock()}>
+			<Button
+				pill
+				class="dark:bg-dark-primary dark:hover:bg-dark-accent bg-[#3BC4A0] hover:bg-[#FF6D2E]"
+				on:click={() => addBlock()}
+			>
 				<ArrowRightOutline />
 			</Button>
 		</ButtonGroup>
@@ -128,21 +169,27 @@
 
 {#if form}
 	{#if form.message.includes('Error')}
-		<Toast color="red" position="bottom-right">
-			<svelte:fragment slot="icon">
-				<CloseCircleSolid class="w-5 h-5" />
-				<span class="sr-only">Error icon</span>
-			</svelte:fragment>
-			{form.message}
-		</Toast>
+		<div
+			class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border-dashed border-4 border-red-500 dark:border-red-500 p-4 mx-auto text-center"
+		>
+			<Card class="text-white bg-red-500 dark:text-white dark:bg-red-500">
+				<p class="p-2">{form.message}</p>
+			</Card>
+		</div>
+		<div class="text-white dark:text-dark-background">
+			{clearForm(2000)}
+		</div>
 	{:else}
-		<Toast color="green" position="bottom-right">
-			<svelte:fragment slot="icon">
-				<CheckCircleSolid class="w-5 h-5" />
-				<span class="sr-only">Error icon</span>
-			</svelte:fragment>
-			{form.message}
-		</Toast>
-		{handleReload()}
+		<div
+			class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border-dashed border-4 border-green-500 dark:border-green-500 p-4 mx-auto text-center"
+		>
+			<Card class="text-white bg-green-500 dark:text-white dark:bg-green-500">
+				<p class="p-2">{form.message}</p>
+			</Card>
+			<Confetti amount={500} x={[-0.5, 3.5]} delay={[0, 2000]} duration={2000} infinite />
+		</div>
+		<div class="text-white dark:text-dark-background">
+			{handleReload()}
+		</div>
 	{/if}
 {/if}
