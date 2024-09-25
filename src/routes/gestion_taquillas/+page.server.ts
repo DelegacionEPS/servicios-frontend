8f3630c7f8779dcb0976022684644084bfcc57a2
manu <100472310@alumnos.uc3m.es>
2024-09-25 12:07:37 +0200

    minor changes+

--- src/routes/gestion_taquillas/+page.server.ts
+++ src/routes/gestion_taquillas/+page.server.ts
@@ -56,7 +56,6 @@ export const actions = {
 		const result = getReservasTaquilla(taquilla);
 		return result;
 	},
-	
 	registerTaquilla: async ({ cookies, request }) => {
 		const data = await request.formData();
 		const taquilla = data.get('taquilla');
