d756ed6c3c3719fbb0f8151224e48e6354e6c0c4
manu <100472310@alumnos.uc3m.es>
2024-10-03 10:14:16 +0200

    fix

--- src/routes/taquillas/+page.svelte
+++ src/routes/taquillas/+page.svelte
@@ -53,10 +53,11 @@
 	<div class="max-w-4xl space-y-4">
 		<Carousel {images} {forward} let:Indicators let:Controls bind:index>
 			<a class="group" slot="slide" href={images[index]?.href} let:Slide let:index>
-				<Slide image={images[index]} />
+				<Slide image={images[index]} href={images[index]?.href}/>
 				<h1
 					class="absolute left-1/2 top-1/2 md:text-2xl sm:text-lg text-sm text-dele-color px-6 py-2 rounded block dark:text-dark-primary"
 					style="transform: translate(-50%, -50%); background-color: rgba(0, 0, 0, 0.8)"
+					href={images[index]?.href}
 				>
 					{titulos[index]}
 				</h1>
