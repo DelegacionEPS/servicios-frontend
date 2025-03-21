88a0083290c7dd28b7d8712787c750c049038e01
Eduardo Alarcón <edualanav@gmail.com>
2025-03-22 00:38:38 +0100

    Add @vercel/speed-insights package for performance insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -39,8 +39,8 @@
 	import type { LayoutData } from './$types';
 	import { _toLeftRightCenter } from 'chart.js/helpers';
 	import { dev } from '$app/environment';
-    
-	
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
 	let hidden2 = true;
 	let transitionParams = {
 		width: 'w-40',
@@ -59,7 +59,7 @@
 	}
 
 	function logout() {
-		signOut(); 
+		signOut();
 		doing_login = false;
 	}
 
@@ -70,31 +70,29 @@
 		let text: String;
 		let inTaquillas = false;
 		let inEdificio = true;
-		let inOsciloscopios = false;		
+		let inOsciloscopios = false;
 		for (let i = 0; i < urlSegments.length; i++) {
 			text = urlSegments[i].charAt(0).toUpperCase() + urlSegments[i].slice(1).replace('_', ' ');
 			if (inTaquillas) {
 				if (inEdificio) {
-					text = "Edificio " + text;
+					text = 'Edificio ' + text;
 					inEdificio = false;
-				}
-				else {
-					text = "Planta " + text;
+				} else {
+					text = 'Planta ' + text;
 				}
 			}
 
 			if (inOsciloscopios) {
-				text = "Puesto " + text;
+				text = 'Puesto ' + text;
 			}
 
-			if (text === "Taquillas") {
+			if (text === 'Taquillas') {
 				inTaquillas = true;
-			}
-			else if (text === "Osciloscopio") {
+			} else if (text === 'Osciloscopio') {
 				inOsciloscopios = true;
-				text = "Puestos"
+				text = 'Puestos';
 			}
-			
+
 			_breadcrumItems.push({
 				text: text,
 				href: `/${urlSegments.slice(0, i + 1).join('/')}`
@@ -130,10 +128,10 @@
 		// Al montar, lee el tema de localStorage (si existe)
 		const savedTheme = localStorage.getItem('theme');
 		if (savedTheme) {
-		theme = savedTheme;
-		document.documentElement.classList.add(theme);
+			theme = savedTheme;
+			document.documentElement.classList.add(theme);
 		} else {
-		document.documentElement.classList.add('light');
+			document.documentElement.classList.add('light');
 		}
 	});
 
@@ -147,7 +145,13 @@
 		localStorage.setItem('theme', newTheme);
 	}
 
-	const correos_premiados = ['100472310@alumnos.uc3m.es', '100472175@alumnos.uc3m.es', '100495832@alumnos.uc3m.es', '100550037@alumnos.uc3m.es', '100522166@alumnos.uc3m.es' ];
+	const correos_premiados = [
+		'100472310@alumnos.uc3m.es',
+		'100472175@alumnos.uc3m.es',
+		'100495832@alumnos.uc3m.es',
+		'100550037@alumnos.uc3m.es',
+		'100522166@alumnos.uc3m.es'
+	];
 </script>
 
 <link
@@ -155,7 +159,9 @@
 	rel="stylesheet"
 />
 
-<body class="dark:bg-dark-background recompensa:bg-recompensa-background selection:bg-[#FFB300] selection:text-white">
+<body
+	class="dark:bg-dark-background recompensa:bg-recompensa-background selection:bg-[#FFB300] selection:text-white"
+>
 	<header
 		class="bg-dele-color grid sm:grid-cols-5 grid-cols-4 gap-x-4 md:gap-x-10 dark:bg-dark-primary recompensa:bg-recompensa-primary navbar sticky top-0 z-50 shadow-md"
 	>
@@ -163,7 +169,11 @@
 			<BarsOutline class="sm:w-10 sm:h-10 w-8 h-8 ml-2" />
 		</button>
 		<a href="/" class="sm:block hidden p-1">
-			<img class="sm:w-12 sm:h-auto w-10 h-auto bg-white dark:bg-dark-primary recompensa:bg-recompensa-primary rounded-xl p-1" src="/eps_logo.png" alt="logo" />
+			<img
+				class="sm:w-12 sm:h-auto w-10 h-auto bg-white dark:bg-dark-primary recompensa:bg-recompensa-primary rounded-xl p-1"
+				src="/eps_logo.png"
+				alt="logo"
+			/>
 		</a>
 		<button
 			class="font-bold-italic text-white text-center py-2 lg:text-2xl sm:text-xl text-base hover:underline w-auto"
@@ -197,26 +207,41 @@
 			{:else}
 				<div></div>
 			{/if}
-			<button on:click={() => login()} class="bg-white rounded-3xl h-10 w-10 place-self-center" id = "pop_account">
+			<button
+				on:click={() => login()}
+				class="bg-white rounded-3xl h-10 w-10 place-self-center"
+				id="pop_account"
+			>
 				<UserCircleOutline class="h-8 w-8 m-auto" />
 			</button>
 		{/if}
 	</header>
 
-	<Popover class="text-black dark:text-white dark:bg-dark-secondary recompensa:bg-recompensa-secondary md:w-1/3 sm:w-1/2 w-10/12 sm:text-md text-sm" title="Cuenta de la UC3M" triggeredBy="#pop_account">
+	<Popover
+		class="text-black dark:text-white dark:bg-dark-secondary recompensa:bg-recompensa-secondary md:w-1/3 sm:w-1/2 w-10/12 sm:text-md text-sm"
+		title="Cuenta de la UC3M"
+		triggeredBy="#pop_account"
+	>
 		<p class=" dark:text-white recompensa:text-white text-sm sm:text-base text-justify">
 			Recuerda iniciar sesión con tu cuenta de la universidad
 		</p>
-	</Popover >
+	</Popover>
 
 	<Breadcrumb
 		solidClass="flex px-5 py-3 text-gray-700 border border-gray-200 rounded-none bg-gray-50 dark:bg-[#111719] dark:border-black recompensa:bg-recompensa-secondary recompensa:border-white recompensa:border-x-0 recompensa:border-t-0"
 		aria-label="Solid background breadcrumb example"
 		solid
 	>
-		<BreadcrumbItem homeClass="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white hover:recompensa:text-white recompensa:text-recompensa-primary" href="/" home>Inicio</BreadcrumbItem>
+		<BreadcrumbItem
+			homeClass="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white hover:recompensa:text-white recompensa:text-recompensa-primary"
+			href="/"
+			home>Inicio</BreadcrumbItem
+		>
 		{#each breadcrumItems as item}
-			<BreadcrumbItem linkClass="ms-1 text-sm font-medium text-gray-700 hover:text-gray-900 md:ms-2 dark:text-gray-400 dark:hover:text-white hover:recompensa:text-white recompensa:text-recompensa-primary" href={item.href}>{item.text}</BreadcrumbItem>
+			<BreadcrumbItem
+				linkClass="ms-1 text-sm font-medium text-gray-700 hover:text-gray-900 md:ms-2 dark:text-gray-400 dark:hover:text-white hover:recompensa:text-white recompensa:text-recompensa-primary"
+				href={item.href}>{item.text}</BreadcrumbItem
+			>
 		{/each}
 	</Breadcrumb>
 
@@ -234,12 +259,22 @@
 			>
 				Menu de Navegación
 			</h5>
-			<CloseButton on:click={() => (hidden2 = true)} class="mb-4 dark:text-white recompensa:text-recompensa-primary" />
+			<CloseButton
+				on:click={() => (hidden2 = true)}
+				class="mb-4 dark:text-white recompensa:text-recompensa-primary"
+			/>
 		</div>
 		<Sidebar>
-			<SidebarWrapper divClass="overflow-y-auto py-4 px-3 rounded dark:bg-dark-secondary recompensa:bg-recompensa-secondary">
+			<SidebarWrapper
+				divClass="overflow-y-auto py-4 px-3 rounded dark:bg-dark-secondary recompensa:bg-recompensa-secondary"
+			>
 				<SidebarGroup>
-					<SidebarItem class="recompensa:text-recompensa-primary hover:recompensa:bg-[#E0E0E0]" label="Inicio" href="/" on:click={() => hideNavBar()}>
+					<SidebarItem
+						class="recompensa:text-recompensa-primary hover:recompensa:bg-[#E0E0E0]"
+						label="Inicio"
+						href="/"
+						on:click={() => hideNavBar()}
+					>
 						<svelte:fragment slot="icon">
 							<HomeSolid
 								class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 recompensa:text-recompensa-primary group-hover:text-gray-900 dark:group-hover:text-white"
@@ -248,7 +283,12 @@
 					</SidebarItem>
 					{#await session then}
 						{#if session?.user?.email != null}
-							<SidebarItem class="recompensa:text-recompensa-primary hover:recompensa:bg-[#E0E0E0]" label="Perfil" href="/perfil" on:click={() => hideNavBar()}>
+							<SidebarItem
+								class="recompensa:text-recompensa-primary hover:recompensa:bg-[#E0E0E0]"
+								label="Perfil"
+								href="/perfil"
+								on:click={() => hideNavBar()}
+							>
 								<svelte:fragment slot="icon">
 									<UserCircleOutline
 										class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 recompensa:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white
@@ -258,7 +298,12 @@
 							</SidebarItem>
 						{/if}
 					{/await}
-					<SidebarItem class="recompensa:text-recompensa-primary hover:recompensa:bg-[#E0E0E0]" label="Taquillas" href="/taquillas" on:click={() => hideNavBar()}>
+					<SidebarItem
+						class="recompensa:text-recompensa-primary hover:recompensa:bg-[#E0E0E0]"
+						label="Taquillas"
+						href="/taquillas"
+						on:click={() => hideNavBar()}
+					>
 						<svelte:fragment slot="icon">
 							<LockOpenOutline
 								class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 recompensa:text-gray-400
@@ -287,7 +332,12 @@
 						{/await}
 					{/await}
 
-					<SidebarItem class="recompensa:text-recompensa-primary hover:recompensa:bg-[#E0E0E0]" label="Puestos de Electrónica" href="/osciloscopio" on:click={() => hideNavBar()}>
+					<SidebarItem
+						class="recompensa:text-recompensa-primary hover:recompensa:bg-[#E0E0E0]"
+						label="Puestos de Electrónica"
+						href="/osciloscopio"
+						on:click={() => hideNavBar()}
+					>
 						<svelte:fragment slot="icon">
 							<DrawSquareOutline
 								class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 recompensa:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white recompensa:group-hover:text-white"
@@ -319,7 +369,12 @@
 						{#await session then}
 							{#if session?.user?.email != null}
 								{#if authorizedEmailsEscuela != null && authorizedEmailsEscuela.includes(session?.user?.email) == true}
-									<SidebarItem class="recompensa:text-recompensa-primary hover:recompensa:bg-[#E0E0E0]" label="Admin" href="/admin" on:click={() => hideNavBar()}>
+									<SidebarItem
+										class="recompensa:text-recompensa-primary hover:recompensa:bg-[#E0E0E0]"
+										label="Admin"
+										href="/admin"
+										on:click={() => hideNavBar()}
+									>
 										<svelte:fragment slot="icon">
 											<UsersSolid
 												class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 recompensa:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white recompensa:group-hover:text-white"
@@ -330,7 +385,12 @@
 							{/if}
 						{/await}
 					{/await}
-					<SidebarItem class="recompensa:text-recompensa-primary hover:recompensa:bg-[#E0E0E0]" label="Encuestas" href="/encuestas" on:click={() => (hidden2 = !hidden2)}>
+					<SidebarItem
+						class="recompensa:text-recompensa-primary hover:recompensa:bg-[#E0E0E0]"
+						label="Encuestas"
+						href="/encuestas"
+						on:click={() => (hidden2 = !hidden2)}
+					>
 						<svelte:fragment slot="icon">
 							<AnnotationSolid
 								class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 recompensa:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white recompensa:group-hover:text-white"
@@ -340,21 +400,29 @@
 
 					<p class="text-center dark:text-white recompensa:text-white">Temas</p>
 
-					{#if correos_premiados && correos_premiados.includes(session?.user?.email)}	
+					{#if correos_premiados && correos_premiados.includes(session?.user?.email)}
 						<div class="grid grid-cols-3 place-items-center">
-							<button on:click={() => setTheme('light')}
-									class="p-2 bg-gray-200 rounded-md"><SunSolid /></button>
-							<button on:click={() => setTheme('dark')}
-									class="p-2 bg-gray-800 text-white rounded-md"><MoonSolid /></button>
-							<button on:click={() => setTheme('recompensa')}
-									class="p-2 bg-yellow-500 text-white rounded-md"><AwardSolid/></button>
+							<button on:click={() => setTheme('light')} class="p-2 bg-gray-200 rounded-md"
+								><SunSolid /></button
+							>
+							<button
+								on:click={() => setTheme('dark')}
+								class="p-2 bg-gray-800 text-white rounded-md"><MoonSolid /></button
+							>
+							<button
+								on:click={() => setTheme('recompensa')}
+								class="p-2 bg-yellow-500 text-white rounded-md"><AwardSolid /></button
+							>
 						</div>
 					{:else}
 						<div class="grid grid-cols-2 place-items-center">
-							<button on:click={() => setTheme('light')}
-									class="p-2 bg-gray-200 rounded-md"><SunSolid /></button>
-							<button on:click={() => setTheme('dark')}
-									class="p-2 bg-gray-800 text-white rounded-md"><MoonSolid /></button>
+							<button on:click={() => setTheme('light')} class="p-2 bg-gray-200 rounded-md"
+								><SunSolid /></button
+							>
+							<button
+								on:click={() => setTheme('dark')}
+								class="p-2 bg-gray-800 text-white rounded-md"><MoonSolid /></button
+							>
 						</div>
 					{/if}
 				</SidebarGroup>
@@ -363,5 +431,5 @@
 	</Drawer>
 
 	<slot />
-	
+	{injectSpeedInsights()}
 </body>
