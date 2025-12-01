acc756cb29f4b2c28a0529e27f1dc357b920f287
Golden <salvadorayalaxd@gmail.com>
2025-12-01 12:25:20 +0100

    Añadido el botón de eliminar reserva oscilo

--- src/routes/perfil/+page.svelte
+++ src/routes/perfil/+page.svelte
@@ -11,12 +11,115 @@
 
     // Reactive statement to update session whenever $page.data.session changes
     $: session = $page.data.session
+    
+    let deleteModal
+    let currentReserva
+    let successToast = false
+    let unSuccessToast = false
+
+    // Esta función asigna la reserva clicada a 'currentReserva' y abre el modal.
+    function openDeleteModal(reserva) {
+        currentReserva = reserva
+        deleteModal = true
+    }
+
+    // Se llama desde el botón de confirmación dentro del Modal.
+    async function eliminar_reserva() {
+        // Call a function that only runs in the server side:
+        let res_email = session?.user?.email || ""
+
+        const response = await fetch("/api/eliminar_reserva_osciloscopio", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                // Usa los datos que están guardados en currentReserva
+                puesto: currentReserva["puesto"],
+                hora: currentReserva["hora"],
+                fecha: currentReserva["fecha"],
+                email: res_email
+            })
+        })
+
+        let result = await response.json()
+        result = result["result"]["message"]
+        if (result.includes("eliminada")) {
+            successToast = true
+            setTimeout(() => {
+                successToast = false
+                location.reload()
+            }, 1000)
+        } else {
+            unSuccessToast = true
+        }
+    }
 </script>
 
 <h1
     class="text-4xl text-center text-dele-color m-5 dark:bg-dark-background dark:text-dark-primary recompensa:bg-recompensa-background recompensa:text-recompensa-primary">
     Perfil
 </h1>
+
+<Modal bind:open={deleteModal} size="md" autoclose={false} class="w-full">
+    <form class="flex flex-col space-y-6">
+        <h3 class="mb-2 text-xl font-medium text-gray-900 dark:text-white recompensa:text-white">
+            Eliminar Reserva
+        </h3>
+        <p>Vas tu reserva:</p>
+        <Label class="space-y-2">
+            <span>NIA:</span>
+            <Input
+                type="text"
+                id="nia"
+                name="nia"
+                value={currentReserva["nia"]}
+                readonly
+                required />
+        </Label>
+        <Label class="space-y-2">
+            <span>Puesto</span>
+            <Input
+                type="text"
+                id="puesto"
+                name="puesto"
+                value={currentReserva["puesto"]}
+                readonly
+                required />
+        </Label>
+        <Label class="space-y-2">
+            <span>Fecha</span>
+            <Input
+                type="text"
+                id="fecha"
+                name="fecha"
+                value={currentReserva["fecha"]}
+                readonly
+                required />
+        </Label>
+        <Label class="space-y-2">
+            <span>Franja Horaria</span>
+            <Input
+                type="text"
+                id="hora"
+                name="hora"
+                value={currentReserva["hora"] + ":00 - " + (currentReserva["hora"] + 2) + ":00"}
+                readonly
+                required />
+        </Label>
+        <p class="font-bold text-xl text-center">Después de eliminarla, deberás reservar otra vez si quieres hacer uso del puesto</p>
+        <Button
+            type="submit"
+            class="w-full1 bg-green-500 hover:bg-dele-accent dark:bg-dark-primary dark:hover:bg-dark-accent recompensa:bg-recompensa-primary hover:recompensa:bg-recompensa-accent"
+            on:click={ev => {
+                ev.preventDefault()
+                deleteModal = false
+                eliminar_reserva()
+            }}>
+            Eliminar Reserva
+        </Button>
+    </form>
+</Modal>
 
 <Tabs
     defaultClass="flex flex-coll space-x-2 rtl:space-x-reverse "
@@ -89,7 +192,7 @@
                             </h5>
                             {#if reserva["estado"] === "reservada"}
                                 <p class="text-center p-1 text-white bg-red-500 rounded">
-                                    Reservada
+                                    Reservado
                                 </p>
                             {:else if reserva["estado"] === "completada"}
                                 <p class="text-center p-1 text-white bg-black rounded">Completa</p>
@@ -102,6 +205,11 @@
                         <p class="text-black text-sm mt-4 dark:text-white recompensa:text-white">
                             Puesto Reservado: {reserva["puesto"]}
                         </p>
+                        <Button
+                            class="mt-4 w-full bg-red-500 hover:bg-red-700"
+                            on:click={() => openDeleteModal(reserva)}>
+                            Eliminar Reserva
+                        </Button>
                     </Card>
                 {/each}
             {:else}
